29befbea2c88e6d232e7ca5064d8dd99026825c1
mpakunderscore <mpakunderscore@gmail.com>
2019-09-11 16:45:11 +0300

    Update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let request = require('request');
 let express = require('express');
 let app = express();
 
@@ -7,9 +8,42 @@ app.use('/', express.static(__dirname + '/'));
 let server = require('http').Server(app);
 server.listen(process.env.PORT || 8080);
 
-let data = {test: true}
+let globalWords = [];
 
 //api
 app.get('/api', function (request, response) {
-    response.json(data);
+
+    response.json({count: globalWords.length, words: globalWords});
 });
+
+let url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&titles=Albert%20Einstein&prop=revisions&rvprop=content'
+
+request(url, function (error, response, body) {
+
+    let responseJson = JSON.parse(body);
+    let text = responseJson.query.pages[Object.keys(responseJson.query.pages)[0]].revisions[0]['*']; // Print the HTML for the Google homepage.
+
+    let words = text.split(' ').reduce((prev, next) => {
+
+        if (next.length === 1)
+            return prev;
+
+        prev[next] = (prev[next] + 1) || 1;
+        return prev;
+
+    }, {});
+
+    let sortable = [];
+    for (let name in words) {
+        let word = {name: name, count: words[name]}
+        sortable.push(word);
+    }
+
+    sortable.sort(function(a, b) {
+        return b['count'] - a['count'];
+    });
+
+    globalWords = sortable;
+});
+
+
