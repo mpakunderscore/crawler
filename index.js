a6400c14a98f6b4ca57eb8330049f4a16328b642
mpakunderscore <mpakunderscore@gmail.com>
2019-09-11 13:43:47 +0300

    Update 2

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ let express = require('express');
 let app = express();
 
 //static
-app.use('/', express.static(__dirname + '/web'));
+app.use('/', express.static(__dirname + '/'));
 
 let server = require('http').Server(app);
 server.listen(process.env.PORT || 8080);
 
+let data = {test: true}
+
 //api
 app.get('/api', function (request, response) {
-    response.json({test: true});
+    response.json(data);
 });
