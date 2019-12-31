033609453ffd48df98198a53b367435bdbc24aea
mpakunderscore <mpakunderscore@gmail.com>
2019-12-31 06:38:13 +0300

    Step inside WIP

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let express = require('express');
 let app = express();
 
 //static
-app.use('/', express.static(__dirname + '/web'));
+app.use('/', express.static(__dirname + '/'));
 
 let server = require('http').Server(app);
 server.listen(process.env.PORT || 8080);
