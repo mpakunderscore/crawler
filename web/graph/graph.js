999c2532f4d22b07b8c8e1eefac589ed5cd76f6e
mpakunderscore <mpakunderscore@gmail.com>
2020-01-10 11:32:31 +0300

    Stable

--- web/graph/graph.js
+++ web/graph/graph.js
@@ -4,74 +4,91 @@ const svg = d3.select("main").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-const simulation = d3.forceSimulation().nodes(nodes_data);
-simulation.force("links", d3.forceLink(links_data)
-    .id(function (d) {
-      return d.id;
-    })
-    .distance(function (d) {
-      return d.value;
-    }));
-simulation.force("charge", d3.forceManyBody())
-simulation.force("center", d3.forceCenter(width / 2, height / 2));
+let link = svg.selectAll("line");
+let node = svg.selectAll("node");
 
-const link = svg.selectAll("line")
-    .data(links_data)
-    .enter()
-    .append("g")
-    .append("line")
-    .attr("class", "link");
-
-let node = svg.selectAll("node")
-    .data(nodes_data)
-    .enter()
-    .append("g")
-    .attr("class", "node")
-    .call(d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended));
-
-node.append("circle")
-    .attr("r", 5)
-    .on("click", function () {
-
-      let title = this.nextSibling.textContent;
-      if (title === '')
-        title = 'Main_topic_classifications';
-
-      d3.select(this).attr("class", "active")
-      d3.select(this.nextSibling).attr("class", "active")
-
-      const response = get('/wiki?title=' + title);
-      const responseJson = JSON.parse(response);
-
-      let new_nodes_data = nodes_data.slice();
-      responseJson.categories.forEach(name => {
-
-        new_nodes_data.push({id: name})
-        // svg.selectAll("line")
-        //     .data(links_data)
-        //     .enter()
-        // links_data.push({source: title, target: name, value: 100})
-      })
-
-      console.log(new_nodes_data)
-
-      node = svg.selectAll("node")
-          .data(new_nodes_data);
-
-    });
-
-node.append("text")
-    .attr("dx", 10)
-    .attr("dy", ".35em")
-    .text(function (d) {
-      return d.id === 'Main' ? '' : d.id
-    });
-
-//add tick instructions:
+let simulation = d3.forceSimulation()
 simulation.on("tick", tickActions);
+
+initData();
+initSimulation();
+
+function initData() {
+
+  node = node.data(nodes_data, function(d) { return d.id });
+
+  node.exit().remove();
+
+  node = node.enter()
+      .append("g")
+      .attr("class", "node")
+      .call(d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended));
+
+  node.append("circle")
+      .attr("r", 5)
+      .on("click", function () {
+
+        let title = this.nextSibling.textContent;
+        if (title === '')
+          title = 'Main_topic_classifications';
+
+        d3.select(this.parent).attr("class", "active")
+        d3.select(this.nextSibling).attr("class", "active")
+
+        const response = get('/wiki?title=' + title);
+        const responseJson = JSON.parse(response);
+
+        console.log(d3.select(this))
+        console.log(links_data)
+        console.log(responseJson.categories)
+
+        // nodes_data = [];
+        // links_data = [];
+
+        responseJson.categories.forEach(name => {
+          nodes_data.push({id: name, group: 1});
+          links_data.push({source: title, target: name, value: 100})
+        })
+
+        console.log(nodes_data)
+        console.log(links_data)
+
+        initData();
+        simulation.force("links").initialize(links_data);
+        simulation.force("charge").initialize(links_data);
+        simulation.force("center").initialize(links_data);
+      });
+
+  node.append("text")
+      .attr("dx", 10)
+      .attr("dy", ".35em")
+      .text(function (d) {
+        return d.id === 'Main_topic_classifications' ? 'Main_topic_classifications' : d.id
+      });
+
+  link = link.data(links_data, function(d) { return d.source.id + "-" + d.target.id; });
+  link.exit().remove();
+  link = link.enter()
+      .append("line")
+      .attr("class", "link")
+      .merge(link);
+}
+
+function initSimulation() {
+  simulation.nodes(nodes_data);
+  simulation.force("links", d3.forceLink(links_data)
+      .id(function (d) {
+        return d.id;
+      })
+      .distance(function (d) {
+        return d.value;
+      }));
+  simulation.force("charge", d3.forceManyBody())
+  simulation.force("center", d3.forceCenter(width / 2, height / 2));
+}
 
 function tickActions() {
   link.attr("x1", function (d) {
@@ -108,12 +125,9 @@ function dragended(d) {
   d.fy = null;
 }
 
-
 function get(theUrl) {
-
   let xmlHttp = new XMLHttpRequest();
   xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
   xmlHttp.send( null );
   return xmlHttp.responseText;
 }
-
--- web/graph/store.js
+++ web/graph/store.js
@@ -4,17 +4,17 @@ const linkLength = 100;
 
 let nodes_data =  [
   {"id": "Main_topic_classifications", "group": 1},
-  {"id": "Economy‎", "group": 1},
-  {"id": "Energy‎", "group": 1},
-  {"id": "Engineering‎", "group": 1},
-  {"id": "Entertainment‎", "group": 1},
-  {"id": "Events‎", "group": 1}
+  // {"id": "Economy‎", "group": 1},
+  // {"id": "Energy‎", "group": 1},
+  // {"id": "Engineering‎", "group": 1},
+  // {"id": "Entertainment‎", "group": 1},
+  // {"id": "Events‎", "group": 1}
 ];
 
 let links_data = [
-  {"source": "Main_topic_classifications", "target": "Economy‎", "value": linkLength},
-  {"source": "Main_topic_classifications", "target": "Energy‎", "value": linkLength},
-  {"source": "Main_topic_classifications", "target": "Engineering‎", "value": linkLength},
-  {"source": "Main_topic_classifications", "target": "Entertainment‎", "value": linkLength},
-  {"source": "Main_topic_classifications", "target": "Events‎", "value": linkLength},
+  // {"source": "Main_topic_classifications", "target": "Economy‎", "value": linkLength},
+  // {"source": "Main_topic_classifications", "target": "Energy‎", "value": linkLength},
+  // {"source": "Main_topic_classifications", "target": "Engineering‎", "value": linkLength},
+  // {"source": "Main_topic_classifications", "target": "Entertainment‎", "value": linkLength},
+  // {"source": "Main_topic_classifications", "target": "Events‎", "value": linkLength},
 ];
