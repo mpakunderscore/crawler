8e7a156353ff8b1b00a094cf90dfdd65bfefcb8e
mpakunderscore <mpakunderscore@gmail.com>
2020-02-08 00:02:36 -0600

    Move wiki out

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const crawler = require("./server/crawler");
+const wiki = require("./server/wiki");
 
 let express = require('express');
 let app = express();
@@ -7,17 +8,20 @@ let app = express();
 app.use('/', express.static(__dirname + '/web'));
 
 let server = require('http').Server(app);
-server.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || 8081);
 
 //api
 app.get('/api', function (request, response) {
     response.json({});
 });
+
 app.get('/url', async function (request, response) {
-    response.json(await crawler.getURLData(request.query.url));
+    const page1 = await crawler.getURLData(request.query.url1);
+    const page2 = await crawler.getURLData(request.query.url2);
+    response.json([page1, page2]);
 });
 
 app.get('/wiki', async function (request, response) {
-    response.json(await crawler.getWikiCategories(request.query.title, request.query.lang));
+    response.json(await wiki.getWikiCategories(request.query.title, request.query.lang));
 });
 
--- server/crawler.js
+++ server/crawler.js
@@ -50,7 +50,7 @@ let getWords = function (text) {
     }
 
     sortable.sort(function(a, b) {
-        return b['count'] - a['count'];
+        return b.count - a.count;
     });
 
     console.log('Words length: ' + sortable.length)
@@ -58,34 +58,4 @@ let getWords = function (text) {
     return sortable;
 }
 
-exports.getWikiCategories = async function (title, lang = 'en') {
-
-    const categoryLang = {en: 'Category:', ru: 'Категория:', simple: 'Category:'}
-    const mainTitle = {en: 'Main_topic_classifications', ru: 'Статьи', simple: 'Articles'};
-
-    try {
-        if (title === 'Wiki') title = mainTitle[lang];
-
-        const urlString = 'https://' + lang + '.wikipedia.org/wiki/' + categoryLang[lang] + title;
-        // console.log(urlString)
-        const url = encodeURI(urlString);
-        const response = await axios.get(url);
-        const data = response.data;
-
-        let categories = [];
-        const $ = cheerio.load(data);
-        $('#mw-subcategories .CategoryTreeItem').find('a').each(function (index, element) {
-            categories.push({id: $(element).text(), info: $(element).next().text()});
-        });
-        let pages = [];
-        $('#mw-pages li').find('a').each(function (index, element) {
-            pages.push({id: $(element).text()});
-        });
-        // console.log(pages)
-
-        return {categories: categories, pages: pages};
-    } catch (error) {
-        console.log(error);
-    }
-}
 
